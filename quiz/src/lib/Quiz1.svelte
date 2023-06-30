cc67e7991a8121d0b0312bb055c45a0f8b0f04e0
apple-x-co <apple.x.co@gmail.com>
2023-06-30 17:31:44 +0900

    chore: ng words

--- quiz/src/lib/Quiz1.svelte
+++ quiz/src/lib/Quiz1.svelte
@@ -5,10 +5,36 @@
 
     onMount(() => window.scrollTo(0, 0))
 
+    const ngWords = ['死']
+    let isValid = true
+    answer.subscribe((obj) => {
+        if (obj.nickname === null) {
+            return
+        }
+
+        isValid = true
+        ngWords.forEach((ngWord) => {
+            if (obj.nickname.indexOf(ngWord) >= 0) {
+                isValid = false
+            }
+        })
+    })
+
     const handleSubmit = () => {
+        if (!isValid) {
+            return
+        }
+
         nextStep()
     }
 </script>
+
+<style>
+    .error {
+        color: red;
+        font-weight: bold;
+    }
+</style>
 
 <div in:fade>
     <h2>step1</h2>
@@ -18,6 +44,9 @@
             ニックネーム
             <input required minlength="1" maxlength="6" autofocus bind:value={$answer.nickname}/>
         </label>
+        {#if isValid === false}
+            <p class="error">別のニックネームを入力してください</p>
+        {/if}
         <button>Next</button>
     </form>
 </div>
